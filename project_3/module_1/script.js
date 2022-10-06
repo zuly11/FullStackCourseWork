c784637961d9814ba33f894674fce513e3ee7fd0
Zuli R <73495176+zuly11@users.noreply.github.com>
2022-10-05 20:51:20 -0500

    enter

---
+++ project_3/module_1/script.js
@@ -0,0 +1,10 @@
+let groceryStoreOpen = false;
+let cornerStoreOpen = false;
+
+if (groceryStoreOpen) {
+  console.log("Go to grocery store");
+} else if (cornerStoreOpen) {
+  console.log("Go to corner store");
+} else {
+  console.log("Guess I'm ordering out tonight");
+}
